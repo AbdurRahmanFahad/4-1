3e51dfbb9dd4e8ae771c0b1193769ba5d332be01
AbdurRahmanFahad <1605069@ugrad.cse.buet.ac.bd>
2021-05-02 03:28:06 +0600

    2nd part init

---
+++ Computer Graphics/Offline 2/Solution/main2.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class point
+{
+public:
+    double x, y, z;
+};
+
+class triangle
+{
+public:
+    point points[3];
+    int color[3];
+};
+
+int main()
+{
+
+    int Screen_Width, Screen_Height, limits;
+    return 0;
+}
