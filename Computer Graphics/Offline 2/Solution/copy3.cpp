b78a6a01bef9efd88d90f63c90782f449fe47b98
AbdurRahmanFahad <1605069@ugrad.cse.buet.ac.bd>
2021-05-05 01:58:05 +0600

    intersection point

--- Computer Graphics/Offline 2/Solution/copy3.cpp
+++ Computer Graphics/Offline 2/Solution/copy3.cpp
@@ -25,26 +25,6 @@ vector<double> left_and_right_intersecting_points(Triangle tr, double py)
     return px;
 }
 
-int x_to_column(double x)
-{
-    return round((x - leftx) / dx);
-}
-
-int y_to_row(double y)
-{
-    return round((topy - y) / dy);
-}
-
-double column_to_x(int c)
-{
-    return leftx + c * dx;
-}
-
-double row_to_y(int r)
-{
-    return topy - r * dy;
-}
-
 void apply_procedure()
 {
     for (int i = 0; i < triangles.size(); i++)
@@ -52,17 +32,11 @@ void apply_procedure()
 
         for (; r >= end_r; r--)
         {
-            double j = row_to_y(r);
             vector<double> temp = left_and_right_intersecting_points(triangles[i], j);
             if (temp.size() < 2)
                 continue;
             left_x = temp[0];
             right_x = temp[1];
-
-            if (left_x < x_left_limit)
-                left_x = x_left_limit;
-            if (right_x > x_right_limit)
-                right_x = x_right_limit;
         }
     }
 }
