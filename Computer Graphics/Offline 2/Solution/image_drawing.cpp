63f59372c3c6198e11bc2c86dd79e3ce05a1c907
AbdurRahmanFahad <1605069@ugrad.cse.buet.ac.bd>
2021-05-05 03:00:09 +0600

    wrap up

--- Computer Graphics/Offline 2/Solution/image_drawing.cpp
+++ Computer Graphics/Offline 2/Solution/image_drawing.cpp
@@ -13,24 +13,6 @@ int main()
 
     bitmap_image image(500, 300);
 
-    // for(int i=0;i<200;i++){
-    //     for(int j=0;j<100;j++){
-    //         image.set_pixel(i,j,255,255,0);
-    //     }
-    //     for(int j=150;j<200;j++){
-    //         image.set_pixel(i,j,0,0,255);
-    //     }
-    // }
-
-    // for(int i=400;i<450;i++){
-    //     for(int j=50;j<150;j++){
-    //         image.set_pixel(i,j,255,0,0);
-    //     }
-    //     for(int j=200;j<300;j++){
-    //         image.set_pixel(i,j,0,255,255);
-    //     }
-    // }
-
     for (int i = 0; i < 500; i++)
     {
         for (int j = 0; j < 300; j++)
@@ -43,7 +25,6 @@ int main()
     }
 
     image.save_image("test.bmp");
-    ;
 
     return 0;
 }
