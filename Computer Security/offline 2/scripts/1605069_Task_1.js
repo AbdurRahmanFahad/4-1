41125c9cd7a403972b535ef4dc4a20640b697e61
AbdurRahmanFahad <1605069@ugrad.cse.buet.ac.bd>
2021-06-21 02:53:23 +0600

    task 1 comment added

--- Computer Security/offline 2/scripts/1605069_Task_1.js
+++ Computer Security/offline 2/scripts/1605069_Task_1.js
@@ -15,10 +15,30 @@
 
     // At first I sent a friend request normally,
     // then inspected the network section in browser's developer tools
-    // then copied the request sent to server when sending friend request 
+    // then copied the request sent to server when sending friend request
     // then I made a request like that using javascript
     // I found a JSON object in source file of the html named elgg
     // I extracted user informations from that JSON object and made a string using that
+
+    /* this is a sample JSON object
+
+    var elgg =
+    {
+        "config":
+    {"lastcache":1549469404,"viewtype":"default","simplecache_enabled":1},
+    "security":
+    {"token":{"__elgg_ts":1624213273,"__elgg_token":"3qiQClD2nrEaJRzoSOYKbg"}},
+    "session":
+    {"user":
+    {"guid":44,"type":"user","subtype":"","owner_guid":44,"container_guid":0,"site_guid":1,"time_created":"2017-07-26T20:29:47+00:00","time_updated":"2021-06-20T18:13:37+00:00","url":"http:\/\/www.xsslabelgg.com\/profile\/alice","name":"Alice","username":"alice","language":"en","admin":false},
+    "token":"V50Hua4Xxja_QWXPPCAVea"
+	        },
+    "_data":{ },
+    "page_owner":
+    {"guid":47,"type":"user","subtype":"","owner_guid":47,"container_guid":0,"site_guid":1,"time_created":"2017-07-26T20:30:59+00:00","time_updated":"2017-07-26T20:30:59+00:00","url":"http:\/\/www.xsslabelgg.com\/profile\/samy","name":"Samy","username":"samy","language":"en"}
+    };
+
+    */
 
     //Create and send Ajax request to add friend
     if(elgg.session.user.guid != userid)
